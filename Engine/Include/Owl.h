8b356f043cf359d340236c9cd227ef0d4d32b9d6
Kellvyn Sampaio <64695440+TheSampaio@users.noreply.github.com>
2023-04-16 09:41:15 -0300

    Update Owl.h

--- Engine/Include/Owl.h
+++ Engine/Include/Owl.h
@@ -5,11 +5,12 @@
 #include "../Source/Owl/Core/Application.h"
 #include "../Source/Owl/Core/Debug.h"
 #include "../Source/Owl/Core/Input.h"
+#include "../Source/Owl/Core/Renderer.h"
 #include "../Source/Owl/Core/Window.h"
 
 // Events
 
+// Game
+
 // Graphics
 #include "../Source/Owl/Graphics/Mesh.h"
-
-// Game
