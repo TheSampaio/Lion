87fba6368a02d6f84238462e01e0bb3f54e9ad7f
Kellvyn Sampaio <64695440+TheSampaio@users.noreply.github.com>
2023-04-26 19:26:34 -0300

    Update Sandbox.cpp

--- Sandbox/Source/Sandbox.cpp
+++ Sandbox/Source/Sandbox.cpp
@@ -33,7 +33,7 @@ void Sandbox::Update(float DeltaTime)
 {
 	// Proccess everything in the game
 	if (GetInput()->GetKeyTapped(Escape)) { GetWindow()->Close(true); }
-	if (GetInput()->GetKeyTapped(F2)) { bWireframe = !bWireframe; GetRenderer()->SetWireframeMode(bWireframe); }
+	if (GetInput()->GetKeyTapped(F2))     { bWireframe = !bWireframe; GetRenderer()->SetWireframeMode(bWireframe); }
 }
 
 void Sandbox::Draw()
