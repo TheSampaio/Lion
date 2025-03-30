55a2ce9d0572d4af37c5b992267ef57b522c12de
Kellvyn Sampaio <mirandasampaiokellvyn@outlook.com>
2025-03-30 20:06:19 -0300

    Added More Submodules
    • Added three new submodules to the project for improved modularity.
    • Resolved warnings in EventDispatcher.h and Engine.h to ensure cleaner builds and better code quality.
    • Refined premake5.lua configuration for better build management and flexibility.

--- Engine/Source/Events/EventDispatcher.h
+++ Engine/Source/Events/EventDispatcher.h
@@ -30,24 +30,21 @@ namespace Lion
         {
             if (m_Event.GetEventType() == T::GetStaticType())
             {
-                // Se a função retornar void
                 if constexpr (std::is_void_v<std::invoke_result_t<Func, T&>>)
-                {
                     func(static_cast<T&>(m_Event));
-                }
-                else // Se a função retornar bool
-                {
+
+                else
                     m_Event.Handled = func(static_cast<T&>(m_Event));
-                }
+
                 return true;
             }
+
             return false;
         }
 
     private:
         Event& m_Event;
 
-        // Usado para obter callbacks de um tipo de evento específico
         template<typename EventType>
         std::vector<std::function<void(const EventType&)>>& GetEventCallbacks()
         {
